5d8af6b58e72122c1e58eceb16deeffffdd062ec
cochindreamdestinations <cochindreamdestinations@gmail.com>
2024-10-08 18:27:17 +0000

    Adding package api

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -8,7 +8,7 @@ import ItenaryTable from '@/components/Tables/TariffTables/ItinaryTable';
 import { TariffTable } from '@/components/Tables/TariffTables/TarrifTableAll';
 
 export default async function Pricing() {
-  const imgres = await fetch('https://cochindreamdestinations.vercel.app/api/public/packages/rentals', {method:'GET'})
+  const imgres = await fetch('https://cochindreamdestinations.vercel.app/api/public/rentals', {method:'GET'})
   const result = await imgres.json();
   
   return (
