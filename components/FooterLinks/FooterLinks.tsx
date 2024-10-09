379d3ad6f3e3c981aada1257813f16ed519a955d
cochindreamdestinations <cochindreamdestinations@gmail.com>
2024-10-09 16:27:05 +0000

    Adding google map iframe

--- components/FooterLinks/FooterLinks.tsx
+++ components/FooterLinks/FooterLinks.tsx
@@ -77,6 +77,17 @@ export function FooterLinks() {
           </Text>
         </div>
         <div className={classes.groups}>{groups}</div>
+        <div className={classes.groups}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15729.999578272675!2d76.3187514!3d9.7236541!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b087b9c1c67e5d5%3A0xbdb32c01d9cd6ac6!2sCochin%20Dream%20Destinations!5e0!3m2!1sen!2sin!4v1728490749188!5m2!1sen!2sin"
+            width="300"
+            height="300"
+            style={{ border: 0 }}
+            allowFullScreen={false}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
       </Container>
       <Container className={classes.afterFooter}>
         <Text c="dimmed" size="sm">
@@ -84,7 +95,6 @@ export function FooterLinks() {
         </Text>
 
         <Group gap={3} className={classes.social} justify="flex-end" wrap="nowrap">
-          
           <ActionIcon
             size="lg"
             color="gray"
@@ -94,7 +104,13 @@ export function FooterLinks() {
             href="https://www.youtube.com/@ReelsOnwheels-he4vf"
             target="_blank"
           >
-            <MImage width={32} height={32} fit="contain" alt='Youtube' src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/youtube" />
+            <MImage
+              width={32}
+              height={32}
+              fit="contain"
+              alt="Youtube"
+              src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/youtube"
+            />
           </ActionIcon>
           <ActionIcon
             size="lg"
@@ -105,8 +121,14 @@ export function FooterLinks() {
             href="https://www.facebook.com/people/Cochin-Dreamdestinations/pfbid0nR9jSFVQ8WvLTkz4YyWhfGVjyz3r6WwmVZUi16vFCs5FsC1dH6of4CNHBAHKVruLl/?mibextid=ZbWKwL"
             target="_blank"
           >
-           <MImage width={32} height={32} fit="contain" alt='Facebook' src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/facebook" />
-           </ActionIcon>
+            <MImage
+              width={32}
+              height={32}
+              fit="contain"
+              alt="Facebook"
+              src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/facebook"
+            />
+          </ActionIcon>
           <ActionIcon
             size="lg"
             color="gray"
@@ -116,20 +138,31 @@ export function FooterLinks() {
             href="https://www.instagram.com/cochindreamdestinations"
             target="_blank"
           >
-            <MImage width={32} height={32} fit="contain" alt='Instagram' src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/instagram" />
-            </ActionIcon>
+            <MImage
+              width={32}
+              height={32}
+              fit="contain"
+              alt="Instagram"
+              src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/instagram"
+            />
+          </ActionIcon>
 
           <ActionIcon
             size="lg"
-            
             variant="subtle"
             aria-label="Trip Advisor"
             component={Link}
             href="https://www.tripadvisor.in/Profile/dreamdestinationscoc"
             target="_blank"
           >
-            <MImage width={32} height={32} fit="contain" alt='Youtube' src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/tripadvisor" />
-            </ActionIcon>
+            <MImage
+              width={32}
+              height={32}
+              fit="contain"
+              alt="Youtube"
+              src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/icons/tripadvisor"
+            />
+          </ActionIcon>
         </Group>
       </Container>
       <Container>
