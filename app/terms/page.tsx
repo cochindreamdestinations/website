17bce529b02e1cf660f49eeec8805e61d3b45739
Abhilash A S <abhilashsajeev@gmail.com>
2024-10-11 18:23:02 +0000

    Adding terms and conditions page

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -3,7 +3,16 @@ import Image from 'next/image';
 export default function TermsPage() {
   return (
     <>
-      <Image src="https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/Logo/kpg21czdrjbzclztwj5r" height={300} width={300} alt="Image background" />
+      <h5  >Important:-</h5>
+      <ul>
+        <li>The duty hours for drivers is from 8.00 AM to 8 PM. These timing are not applicable for arrivals/pick up.
+        </li><li>Any complaints/issues related to the driver to be intimated to our representative promptly during the service itself so that we can take necessary actions to yield a better service.
+        </li><li>In the event of circumstances/situations such as road blockage due to strike/agitation, road construction, natural calamities, sickness evacuation, delay/cancellation of train/flight etc. Also, due to monsoon rains and extreme weather patterns journey time can vary enormously at all times of year, which is beyond our control and additional expenses incurred due to these reasons has to be borne by the agent / client.
+        </li><li>Vomiting on seats and floor are subjected to additional cleaning charges payable at nearby service centres directly by the passenger.
+        </li><li>Consumption of alcohol/smoking is strictly prohibited inside the vehicles.
+        </li><li>Sedan cars are not equipped with luggage carrier on top. Only bigger cars are equipped with luggage carrier on top such as Ertiga/Lodgy/Innova, etc.
+        </li><li>Rates are quoted in INR (Indian Rupees)</li>
+      </ul>
     </>
   );
 }
