97ab94373eef6b60736dee4991b1efeb9556e021
Abhilash A S <abhilashsajeev@gmail.com>
2024-10-17 12:12:55 +0000

    new sitemap and titile for home page

--- app/page.tsx
+++ app/page.tsx
@@ -66,6 +66,52 @@ const taxiData = [
   },
 ];
 
+
+export const metadata = {
+  title: 'Kerala Taxi - Kerala Taxi Packages - Kochi Taxi| Cochin Dream Destinations ',
+  description: 'Kerala Taxi - Kerala Taxi Packages - Kochi Taxi Cochin dream destinations',
+  twitter: {
+    card: 'https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/Logo/kpg21czdrjbzclztwj5r',
+    url: 'https://cochindreamdestinations.in',
+    title: 'Kerala Taxi - Kerala Taxi Packages - Kochi Taxi | Cochin Dream Destinations | Kerala Taxi Cab Services | Other Links',
+    description: 'Kerala Taxi - Kerala Taxi Packages - Kochi Taxi taxi service packages with Cochin dream destinations',
+  },
+  openGraph: {
+    title: 'Kerala Taxi - Kerala Taxi Packages - Kochi Taxi | Cochin Dream Destinations | Kerala Taxi Cab Services',
+    description: 'List of available taxi service packages with Cochin dream destinations',
+    url: 'https://cochindreamdestinations.in',
+    siteName: 'Cochin Dream Destinations Taxi Service',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/Logo/kpg21czdrjbzclztwj5r', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://res.cloudinary.com/dtgoc3cww/image/upload/f_auto,q_auto/v1/Logo/kpg21czdrjbzclztwj5r', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    videos: [
+      {
+        url: 'https://nextjs.org/video.mp4', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    audio: [
+      {
+        url: 'https://nextjs.org/audio.mp3', // Must be an absolute URL
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
+
+
 export default async function HomePage() {
   const reviews = await getReviews();
 
